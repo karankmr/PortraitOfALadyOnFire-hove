947ecc3fa9902e97930f6effac1f50740d9636f8
Karan Kumar <karankmr987.kk@gmail.com>
2020-05-04 09:17:18 +0100

    First Commit

---
+++ src/effects/effects.js
@@ -0,0 +1,341 @@
+const button1 = new hoverEffect({
+    parent: document.querySelector('.div-p1'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-1.1.png',
+    image2: 'poalof-images/img-1.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+
+const button2 = new hoverEffect({
+    parent: document.querySelector('.div-p2'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-2.1.png',
+    image2: 'poalof-images/img-2.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+
+
+const button3 = new hoverEffect({
+    parent: document.querySelector('.div-p3'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-3.1.png',
+    image2: 'poalof-images/img-3.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+// const button4 = new hoverEffect({
+//     parent: document.querySelector('.div-p4'),
+//     intensity1: 0.25,
+//     intensity2: 0.25,
+//     speedIn: 1.9,
+//     angle1: Math.PI / 2,
+//     angle2: -Math.PI / 2,
+//     image1: 'poalof-images/img-4.1.png',
+//     image2: 'poalof-images/img-4.2.png',
+//     displacementImage: 'images/heightMap.jpg'
+// });
+
+const button5 = new hoverEffect({
+    parent: document.querySelector('.div-p5'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-5.1.png',
+    image2: 'poalof-images/img-5.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+const button6 = new hoverEffect({
+    parent: document.querySelector('.div-p6'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-6.1.png',
+    image2: 'poalof-images/img-6.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+const button7 = new hoverEffect({
+    parent: document.querySelector('.div-p7'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-7.1.png',
+    image2: 'poalof-images/img-7.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+// const button8 = new hoverEffect({
+//     parent: document.querySelector('.div-p8'),
+//     intensity1: 0.25,
+//     intensity2: 0.25,
+//     speedIn: 1.9,
+//     angle1: Math.PI / 2,
+//     angle2: -Math.PI / 2,
+//     image1: 'poalof-images/img-8.1.png',
+//     image2: 'poalof-images/img-8.2.png',
+//     displacementImage: 'images/heightMap.jpg'
+// });
+
+const button9 = new hoverEffect({
+    parent: document.querySelector('.div-p9'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-9.2.png',
+    image2: 'poalof-images/img-9.1.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+const button10 = new hoverEffect({
+    parent: document.querySelector('.div-p10'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-10.1.png',
+    image2: 'poalof-images/img-10.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+const button11 = new hoverEffect({
+    parent: document.querySelector('.div-p11'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-11.1.png',
+    image2: 'poalof-images/img-11.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+// const button12 = new hoverEffect({
+//     parent: document.querySelector('.div-p12'),
+//     intensity1: 0.25,
+//     intensity2: 0.25,
+//     speedIn: 1.9,
+//     angle1: Math.PI / 2,
+//     angle2: -Math.PI / 2,
+//     image1: 'poalof-images/img-12.1.png',
+//     image2: 'poalof-images/img-12.2.png',
+//     displacementImage: 'images/heightMap.jpg'
+// });
+
+const button13 = new hoverEffect({
+    parent: document.querySelector('.div-p13'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-13.1.png',
+    image2: 'poalof-images/img-13.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+const button14 = new hoverEffect({
+    parent: document.querySelector('.div-p14'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-14.1.png',
+    image2: 'poalof-images/img-14.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+const button15 = new hoverEffect({
+    parent: document.querySelector('.div-p15'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-15.1.png',
+    image2: 'poalof-images/img-15.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+const button16 = new hoverEffect({
+    parent: document.querySelector('.div-p16'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-16.1.png',
+    image2: 'poalof-images/img-16.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+const button17 = new hoverEffect({
+    parent: document.querySelector('.div-p17'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-17.1.png',
+    image2: 'poalof-images/img-17.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+
+const button18 = new hoverEffect({
+    parent: document.querySelector('.div-p18'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-18.1.png',
+    image2: 'poalof-images/img-18.2.png',
+    displacementImage: 'images/stripe1mul.jpg'
+});
+//
+const button19 = new hoverEffect({
+    parent: document.querySelector('.div-p19'),
+    intensity1: 0.25,
+    intensity2: 0.25,
+    speedIn: 1.9,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'poalof-images/img-19.1.png',
+    image2: 'poalof-images/img-19.2.png',
+    displacementImage: 'images/heightMap.jpg'
+});
+// const button20 = new hoverEffect({
+//     parent: document.querySelector('.div-p20'),
+//     intensity1: 0.25,
+//     intensity2: 0.25,
+//     speedIn: 1.9,
+//     angle1: Math.PI / 2,
+//     angle2: -Math.PI / 2,
+//     image1: 'poalof-images/img-20.1.png',
+//     image2: 'poalof-images/img-20.2.png',
+//     displacementImage: 'images/heightMap.jpg'
+// });
+
+
+
+
+
+
+
+
+
+// const button3 = new hoverEffect({
+//     parent: document.querySelector('.div-p2'),
+//     intensity1: 0.25,
+//     intensity2: 0.25,
+//     speedIn: 1.9,
+//     angle1: Math.PI / 2,
+//     angle2: -Math.PI / 2,
+//     image1: 'poalof-images/img-2.1.png',
+//     image2: 'poalof-images/img-2.2.png',
+//     displacementImage: 'images/heightMap.jpg'
+// });
+//
+// var button4 = new hoverEffect({
+//     parent: document.querySelector('.div-p9'),
+//     intensity1: 0.25,
+//     intensity2: 0.25,
+//     speedIn: 1.8,
+//     angle1: Math.PI / 2,
+//     angle2: -Math.PI / 2,
+//     image1: 'poalof-images/img-10.2.png',
+//     image2: 'poalof-images/img-10.1.png',
+//     displacementImage: 'images/ramen.jpg'
+// });
+//
+//
+//
+// var button4 = new hoverEffect({
+//     parent: document.querySelector('.div-video'),
+//     image1: 'videos/video.mp4',
+//     image2: 'videos/video2.mp4',
+//     displacementImage: 'images/fluid.jpg',
+//     video: true
+// });
+//
+// var button5 = new hoverEffect({
+//     parent: document.querySelector('.div'),
+//     intensity1: 0.1,
+//     intensity2: 0.1,
+//     angle2: Math.PI / 2,
+//     image1: 'images/ny.jpg',
+//     image2: 'images/ny-info.jpg',
+//     imagesRatio: 512 / 512,
+//     displacementImage: 'images/stripe1mul.png'
+// });
+//
+// var button6 = new hoverEffect({
+//     parent: document.querySelector('.div2'),
+//     intensity1: 0.25,
+//     intensity2: 0.25,
+//     angle1: Math.PI / 2,
+//     angle2: -Math.PI / 2,
+//     image1: 'images/ice.jpg',
+//     image2: 'images/ice2.jpg',
+//     displacementImage: 'images/ramen.jpg'
+// });
+//
+// var button7 = new hoverEffect({
+//     parent: document.querySelector('.div3'),
+//     intensity: 0.3,
+//     image1: 'images/hover.jpg',
+//     image2: 'images/hover.jpg',
+//     displacementImage: 'images/fluid.jpg',
+//     speedIn: 1.0,
+//     speedOut: 1.0
+// });
+//
+// var button8 = new hoverEffect({
+//     parent: document.querySelector('.div4'),
+//     intensity: 0.1,
+//     image1: 'images/rect.png',
+//     image2: 'images/rect.png',
+//     displacementImage: 'images/strip.png'
+// });
+//
+// var button9 = new hoverEffect({
+//     parent: document.querySelector('.div5'),
+//     intensity: 0.2,
+//     imagesRatio: 512 / 768,
+//     image1: 'images/balloon.jpg',
+//     image2: 'images/balloon2.jpg',
+//     displacementImage: 'images/dot.jpg',
+//     hover: false
+// });
+// document.querySelector('.next').addEventListener('click', button5.next);
+// document.querySelector('.previous').addEventListener('click', button5.previous);
+//
+// var button10 = new hoverEffect({
+//     parent: document.querySelector('.div6'),
+//     intensity: 0.2,
+//     image1: 'images/submit.png',
+//     image2: 'images/submit.png',
+//     displacementImage: 'images/heightMap.png'
+// });
